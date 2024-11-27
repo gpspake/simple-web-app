115b25ae248d9c8626b8bb6263ee1ca4be5206f3
George Spake <george@georgespake.com>
2024-11-26 20:45:53 -0600

    🔨 add code to run migrations
    add data.db to gitignore

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Open SQLite database
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	// Verify the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to SQLite: %v", err)
+	}
+
+	defer db.Close()
+
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+
+	if err != nil {
+		log.Fatalf("Could not create SQLite driver: %v", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"sqlite3",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatalf("Could not initialize migrations: %v", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Could not run migrations: %v", err)
+	}
+
+	log.Println("Migrations applied successfully!")
 }
